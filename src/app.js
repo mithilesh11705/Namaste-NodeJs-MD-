3148e7980ec3739e4a34e270df6c9c9f78c9dcb0
mithilesh11705 <mithileshdeore7@gmail.com>
2025-04-02 16:51:29 +0530

    MD

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
-const  express=require("express");
+const express = require("express");
 
-const app=express(); 
+const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the sever");
+app.use("/test", (req, res) => {
+  res.send("Hello from the sever");
 });
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Mithilesh", LastName: "Deore" });
 });
 
-app.listen(3000,()=>{
-    console.log("Server is successfully on port on 3000...");
+app.delete("/user", (req, res) => {
+  res.send("User deleted");
 });
 
- 
+app.patch("/user", (req, res) => {
+  res.send("User updated");
+});
 
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to DB");
+});
 
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully on port on 3000...");
+});
