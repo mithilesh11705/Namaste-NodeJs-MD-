c6a76df1d19d1af9b253be05303326e77acec9f4
mithilesh11705 <mithileshdeore7@gmail.com>
2025-04-04 13:53:46 +0530

    Feed API

--- src/app.js
+++ src/app.js
@@ -4,13 +4,34 @@ const app = express();
 const User = require("./models/user");
 const connectDB = require("./config/database.js");
 
+app.use(express.json()); //json middelware
+
+//get user by  email
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (user.length === 0) res.status(404).send("User Not Found");
+    else res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Feed API- get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Mithilesh",
-    lastName: "Kumar",
-    email: "mithilesh@gmail.com",
-    password: "123456",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User created");
